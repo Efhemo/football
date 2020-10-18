026f00a8b28b20ecccd481cf3ec0398f628bc6e4
Adegbite Femi <adegbitefemisamson@gmail.com>
2020-10-18 12:38:38 +0100

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:football/config/config.dart';
 import 'package:football/config/theme.dart';
-import 'package:football/profile_provider.dart';
-import 'package:provider/provider.dart';
 import 'screens/screen.dart';
 import 'package:theme_mode_handler/theme_mode_handler.dart';
 
@@ -21,12 +19,10 @@ class MyApp extends StatelessWidget {
           darkTheme: ThemeData.dark().copyWith(
             scaffoldBackgroundColor: Colors.black,
             appBarTheme: Styles.appBarTheme,
-//            textTheme: Styles.textTheme
           ),
           theme: ThemeData.light().copyWith(
             primaryColor: Colors.blue,
             appBarTheme: Styles.appBarTheme,
-//            textTheme: Styles.textTheme,
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
           themeMode: themeMode,
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,9 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:football/data/data.dart';
-import 'package:football/profile_provider.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:theme_mode_handler/theme_mode_handler.dart';
 
 class ProfileScreen extends StatelessWidget {
