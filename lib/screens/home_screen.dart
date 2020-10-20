b42bec0c6bbeac486a83e173ccb84995501ba26a
Adegbite Femi <adegbitefemisamson@gmail.com>
2020-10-20 03:39:27 +0100

    build bottom navscreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:football/screens/screen.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +8,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _destinations = [
+    FootballScreen(),
+    Scaffold(),
+    Scaffold(),
+    ProfileScreen()
+  ];
+
+  final _bottomNavItem = {
+    "Football": MdiIcons.soccer,
+    "News": Icons.chat_bubble_outline,
+    "Bookmark": Icons.done_outline,
+    "Profile": Icons.person_outline
+  };
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _destinations,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 28.0,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        items: _bottomNavItem
+            .map((title, icon) => MapEntry(
+                title,
+                BottomNavigationBarItem(
+                  icon: Icon(icon),
+                  title: Text(title)
+                )))
+            .values
+            .toList(),
+      ),
+    );
   }
 }
