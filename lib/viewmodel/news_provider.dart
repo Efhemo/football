a823ca171db7074a0b3b205efddf3eadb7304d60
Adegbite Femi <adegbitefemisamson@gmail.com>
2020-12-01 17:44:18 +0100

    updated readme

--- lib/viewmodel/news_provider.dart
+++ lib/viewmodel/news_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:football/model/news.dart';
 import 'base_viewmodel.dart';
@@ -6,9 +8,29 @@ import 'package:football/domain/domain.dart';
 class NewsProvider extends BaseViewModel<News> {
   final NewsRepository newsRepository;
 
+  StreamController<List<News>> _streamController = StreamController();
+  Stream<List<News>> get newsStream => _streamController.stream;
 
   NewsProvider({@required this.newsRepository}){
     fetchNews();
+
+    //watchNews();
+  }
+
+  //not working
+  void watchNews() {
+    var listNews = List<News>();
+
+    newsRepository.watchNews().listen((news) {
+      listNews.add(news);
+      print("news title is ${news.title}");
+    }, onDone: () { // Not excecuting
+      _streamController.sink.add(listNews);
+      print("onDone is ${listNews.length}");
+      listNews = List<News>();
+    }, onError: (error) {
+      print("on error is ${error}");
+    });
   }
 
   void fetchNews() async {
@@ -19,5 +41,11 @@ class NewsProvider extends BaseViewModel<News> {
             (r) => setError(null)
     );
     setViewState(ViewState.loaded);
+  }
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
   }
 }
